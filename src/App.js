cb226090d4f34f1e7573233f2cf6171b23839643
kristaiida <92725476+kristaiida@users.noreply.github.com>
2022-01-25 20:38:46 +0200

    List exercise

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const names = ['Jouni', 'Pekka', 'Ilkka'];
+    setItems(names);
+    console.log(items);
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin: '30px'}}>
+      <h3>Names</h3>
+      <ul>
+        {items.map(item => (
+          <li key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
